b7c9858a89741ce3b489da1e707e2a059bb9ca4b
Patryk Węgrzyński <wegosh16@gmail.com>
2025-02-01 03:36:41 +0000

    up

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          // Light Mode Colors
+          "light-gray": "#F5F5F5",
+          "muted-gray": "#6C757D",
+          "deep-blue": "#003366",
+          gold: "#FFD700",
+          // Dark Mode Colors
+          charcoal: "#1A1A1A",
+          "muted-beige": "#A8A8A8",
+          "emerald-green": "#008080",
+          "bright-orange": "#FF8C42",
+        },
+      },
+    },
+    plugins: [],
+  };
